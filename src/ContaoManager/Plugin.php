e8ecbc520e174faf2dbaa2267f030080bb4938ac
Rory Zünd <mail@rory.ch>
2020-01-05 17:21:01 +0100

    added Contao Manager Plugin

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright  Media Motion AG <https://www.mediamotion.ch>
+ * @author     Rory Zünd (Media Motion AG)
+ * @package    SyslogBundle
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/mediamotionag/contao-syslog-bundle
+ */
+
+
+namespace Memo\SyslogBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Memo\SyslogBundle\SyslogBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(SyslogBundle::class)->setLoadAfter([ContaoCoreBundle::class])
+        ];
+    }
+}
